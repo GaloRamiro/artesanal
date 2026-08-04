4d421b05f4b111ffe915619d92e55e9d5db2f87d
GaloRamiro <g_cisneros_23@hotmail.es>
2026-08-04 17:02:25 -0500

    Solución a NullPointerException

--- src/com/krakedev/artesanal/testNegocio/TestNegocio.java
+++ src/com/krakedev/artesanal/testNegocio/TestNegocio.java
@@ -7,12 +7,12 @@ public class TestNegocio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Negocio negocio1 = new Negocio();
+		Maquina nueva = new Maquina("CO123", "Club", "Cerveza Fria", 0.02, 8000);
+		Negocio negocio1 = new Negocio("Artica", nueva);
 
 		System.out.println("Nombre: " + negocio1.getNombre());
 		System.out.println("Maquina: " + negocio1.getMaquinaA());
-		
+
 		Maquina m1 = negocio1.getMaquinaA();
 		double capacidad = m1.getCapacidadMaxima();
 
