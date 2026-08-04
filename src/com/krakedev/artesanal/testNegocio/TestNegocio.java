b3149877ed69e89c919f9c5f94e76a65227b48c9
GaloRamiro <g_cisneros_23@hotmail.es>
2026-08-04 16:58:00 -0500

    nullPointerException

--- src/com/krakedev/artesanal/testNegocio/TestNegocio.java
+++ src/com/krakedev/artesanal/testNegocio/TestNegocio.java
@@ -1,5 +1,6 @@
 package com.krakedev.artesanal.testNegocio;
 
+import com.krakedev.artesanal.Maquina;
 import com.krakedev.artesanal.Negocio;
 
 public class TestNegocio {
@@ -7,10 +8,14 @@ public class TestNegocio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Negocio negocio1= new Negocio();
+		Negocio negocio1 = new Negocio();
+
+		System.out.println("Nombre: " + negocio1.getNombre());
+		System.out.println("Maquina: " + negocio1.getMaquinaA());
 		
-		System.out.println("Nombre: "+negocio1.getNombre());
-		System.out.println("Maquina: "+negocio1.getMaquinaA());
+		Maquina m1 = negocio1.getMaquinaA();
+		double capacidad = m1.getCapacidadMaxima();
+
 	}
 
 }
